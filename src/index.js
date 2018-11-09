34aa30a9241eb52a211ad404707dc560c2158479
김진우 <jw.kim@nhnent.com>
2018-11-09 18:58:44 +0900

    apply middle commit

--- src/index.js
+++ src/index.js
@@ -1,77 +1,9 @@
-import Vue from 'vue';
-import PortalVue from 'portal-vue';
+import '../assets/css/common.css';
+import '../assets/css/example.css';
 
-import store from './store';
-
-import {setGlobalOptions} from '@/helpers/globalOptions';
-
-import globalKeymap from '@/plugins/globalKeymap';
-
-import './directives';
-import '../assets/css/import_new.css';
-import '../assets/css/edit_new.css';
-import '../assets/css/fe-override.css';
-
-import '../node_modules/tui-color-picker/dist/tui-color-picker.css';
-import '../node_modules/swiper/dist/css/swiper.min.css';
-
-import App from './components/App.vue';
-import * as serverStub from '@/helpers/serverStub';
-
-import {
-  updateSnsState,
-  updateBoardState,
-  updateMapState,
-  updateProductCategoryState,
-  updateProductSectionState
-} from '@/store/types';
-
-// Portal Vue - https://linusborg.github.io/portal-vue/#/guide
-Vue.use(PortalVue);
-Vue.config.productionTip = false;
-
-Vue.use(globalKeymap);
-
-/**
- * Get new instance
- * @param {object} options options
- * @returns {object}
- */
-export function wysiwygContentsEditor(options) {
-  setGlobalOptions(options);
-
-  const vm = new Vue({
-    el: options.el,
-    store,
-    render: h => h(App)
-  });
-
-  return {
-    vm,
-    updateSnsState(state) {
-      store.commit(updateSnsState, state);
-    },
-    updateBoardState(state) {
-      store.commit(updateBoardState, state);
-    },
-    updateMapState(state) {
-      store.commit(updateMapState, state);
-    },
-    updateProductCategoryState(state) {
-      store.commit(updateProductCategoryState, state);
-    },
-    updateProductSectionState(state) {
-      store.commit(updateProductSectionState, state);
+export class HelloWorld {
+    constructor(element) {
+        console.log('a', element);
     }
-  };
 }
 
-if (process.env.NODE_ENV === 'development') {
-  const el = document.createElement('div');
-  document.body.insertBefore(el, document.body.firstChild);
-
-  window.wce = wysiwygContentsEditor({
-    el,
-    ...serverStub
-  });
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = (env, options = {}) => {
   const config = {
     entry: {
-      app: ['@babel/polyfill', './src/index.js']
+      app: ['./src/index.js']
     },
     output: {
       filename: '[name].bundle.js',
@@ -58,8 +60,7 @@ module.exports = (env, options = {}) => {
     config.plugins = [
       new webpack.HotModuleReplacementPlugin(),
       new HtmlWebpackPlugin({
-        title: 'Development',
-        showErrors: true
+        template: 'example/index.html'
       })
     ];
 
@@ -73,18 +74,21 @@ module.exports = (env, options = {}) => {
       }
     };
   } else {
+    config.optimization = {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    };
+
     config.plugins = [
       new CleanWebpackPlugin(['dist'])
     ];
   }
-
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(options.mode)
-      }
-    })
-  );
 
   config.plugins.push(new MiniCssExtractPlugin());
 
