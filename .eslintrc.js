540c3924a379444596f91483bd09d9d31cc712f6
김진우 <jw.kim@nhnent.com>
2019-01-02 14:42:31 +0900

    add files for review

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['tui', 'plugin:prettier/recommended'],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    indent: [2, 2, {SwitchCase: 1, ignoreComments: false, ImportDeclaration: 1, flatTernaryExpressions: false}]
+  }
+};
